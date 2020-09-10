a8f0045267c96df75126f8ccb65693dd2817d4bd
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-09-10 20:40:26 +0530

    Update port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const admin = require("./route/admin");
 const cart = require("./route/cart");
 const order = require("./route/order");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
